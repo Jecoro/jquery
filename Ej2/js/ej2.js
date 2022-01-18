5c6fb65c6df439a3fc96a0370ff1c0da4d1bf2b8
Jecoro <jecora98@hotmail.com>
2022-01-18 12:25:20 +0100

    Update Ej2

---
+++ Ej2/js/ej2.js
@@ -0,0 +1,5 @@
+function imagenAleatoria(){
+    var url = "http://lorempixel.com/400/400/"
+    document.getElementsByTagName("img")[0].src=url;
+    //document.getElementsByTagName("img")[0].setAttribute("src",url)
+}
